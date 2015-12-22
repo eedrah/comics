b10a07efa62981b13babec06dc02410a12653ab9
Eedrah <eedrah@users.noreply.github.com>
2015-12-23 03:28:23 +1300

    show multiple comics for mobile view

--- src/components/root.js
+++ src/components/root.js
@@ -5,6 +5,13 @@ var comic = require('./comic')
 
 var className = sheetify('./root.css')
 
+let comics = []
+for (var i = 1; i <= 20; i++) {
+  comics.push(comic(i))
+}
+
 module.exports = function () {
-  return h('div', {className}, comic(1))
+  return h('div', {className},
+    comics
+  )
 }
