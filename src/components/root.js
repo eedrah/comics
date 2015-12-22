e6781452ab754f8461e9ebb5cf53a49e2fc4e3d7
Eedrah <eedrah@users.noreply.github.com>
2015-12-23 02:38:43 +1300

    make loading component

--- src/components/root.js
+++ src/components/root.js
@@ -2,9 +2,13 @@ var h = require('virtual-dom/h')
 var sheetify = require('sheetify')
 
 var comic = require('./comic')
+var loader = require('./loader')
 
 var className = sheetify('./root.css')
 
 module.exports = function () {
-  return h('div', {className}, comic(1))
+  return h('div', {className}, [
+    comic(1),
+    loader()
+  ])
 }
